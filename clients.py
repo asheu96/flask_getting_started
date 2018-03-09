93fa7ad11cecd7fd98b03143cf3767a933181a4a
VCM admin account <vcm@vcm-3579.vm.duke.edu>
2018-03-08 21:59:17 -0500

    added revised clients, flask_mini file. added simple distance test script

--- clients.py
+++ clients.py
@@ -1,2 +1,26 @@
 import requests
-r = requests.get('vcm-3579.vm.duke.edu:5000')
+import sys
+
+def callReq():
+
+    """ callReq collects all of the API requests
+    Does not return a value, void method"""
+
+    nameFunc = requests.get('http://vcm-3579.vm.duke.edu:5000/name')
+    name = nameFunc.json()
+    sys.stdout.write(str(name) + '\n')
+
+    hello = requests.get('http://vcm-3579.vm.duke.edu:5000/hello/AlexSheu')
+    hello = hello.json()
+    sys.stdout.write(str(hello) + '\n')
+
+    dist = requests.post('http://vcm-3579.vm.duke.edu:5000/distance', json={'a':[4,5], 'b':[3,4]} )
+    dist = dist.json()
+    sys.stdout.write(str(dist) + '\n')
+
+if __name__ == "__main__":
+	callReq()
+
+
+
+
---
+++ test_distance.py
@@ -0,0 +1,5 @@
+def test_distance():
+
+    """Simple test that checks if the input distance calculations are done correctly"""
+    from flask_mini import returnDist
+    assert returnDist() == pytest.approx(1.41)
